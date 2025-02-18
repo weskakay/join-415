63cf6136495405dd7607402eb0da7e55dcb5d691
Valentino Strebel <tino_schweizer@hotmail.com>
2025-02-18 09:03:13 +0100

    added more data to db.js

--- js/db.js
+++ js/db.js
@@ -23,6 +23,19 @@ let months = [
   "December",
 ];
 
+let bgcolors = [
+  { id: 0, rgba: "rgba(255, 105, 135, 1)" },
+  { id: 1, rgba: "rgba(255, 180, 120, 1)" },
+  { id: 2, rgba: "rgba(186, 85, 211, 1)" },
+  { id: 3, rgba: "rgba(100, 200, 250, 1)" },
+  { id: 4, rgba: "rgba(60, 179, 113, 1)" },
+  { id: 5, rgba: "rgb(153, 197, 43)" },
+  { id: 6, rgba: "rgba(218, 165, 32, 1)" },
+  { id: 7, rgba: "rgb(205, 127, 224)" },
+  { id: 8, rgba: "rgba(138, 43, 226, 1)" },
+  { id: 9, rgba: "rgba(255, 165, 0, 1)" },
+];
+
 async function loadDataBoard() {
   await getContacts();
   await getTasks();
