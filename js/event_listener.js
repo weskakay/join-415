28acfb31be278d4d93cbcb9f549c741b476444be
Vale Cannelloni <tino_schweizer@hotmail.com>
2025-02-26 19:30:05 +0100

    fixed event listener for login

--- js/event_listener.js
+++ js/event_listener.js
@@ -2,14 +2,16 @@ let subtaskRef = document.getElementById("subtaskInput");
 let contactsInputRef = document.getElementById("contact-search");
 let contactsCheckRef = document.getElementById("contacts-checkbox");
 
-document.addEventListener("click", function (event) {
-  let list = document.getElementById("contacts-checkbox");
-  let searchBox = document.getElementById("contact-search");
+if (window.location.pathname.includes("board.html")) {
+  document.addEventListener("click", function (event) {
+    let list = document.getElementById("contacts-checkbox");
+    let searchBox = document.getElementById("contact-search");
 
-  if (!list.contains(event.target) && event.target !== searchBox) {
-    list.style.display = "none";
-  }
-});
+    if (!list.contains(event.target) && event.target !== searchBox) {
+      list.style.display = "none";
+    }
+  });
+}
 
 if (subtaskRef) {
   subtaskRef.addEventListener("keydown", function (event) {
